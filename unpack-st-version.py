8bec922549a075bd3ee041c0de721fb2ab026d37
Jim Lawton <jim.lawton@gmail.com>
2015-04-21 16:17:48 +0100

    Find all Sublime packages.

--- unpack-st-version.py
+++ unpack-st-version.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 #
 # Simple script to unpack a Sublime-Text 3 install kit into a tree structure 
 # that can be committed to Git, so that different versions of Sublime Text 
@@ -12,7 +12,19 @@ import shutil
 
 
 def main():
-    pass
+    excludes = [".git"]
+    for dirpath, dirnames, filenames in os.walk(os.getcwd()):
+        if os.path.dirname(dirpath) in excludes:
+            dirnames[:] = []
+            filenames[:] = []
+            continue
+        for x in excludes:
+            if x in dirnames:
+                del dirnames[dirnames.index(x)]
+        for filename in filenames:
+            if filename.endswith(".sublime-package"):
+                path = os.path.join(dirpath, filename)
+                print path
 
 
 if __name__ == '__main__':
