de1fd511f48f14532d763d1a7a417f99a8e42d31
Jim Lawton <jim.lawton@gmail.com>
2015-04-21 14:52:00 +0100

    Skeleton unpacker script.

---
+++ unpack-st-version.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+#
+# Simple script to unpack a Sublime-Text 3 install kit into a tree structure 
+# that can be committed to Git, so that different versions of Sublime Text 
+# can be compared.
+#
+# Jim Lawton, April 21st, 2015.
+
+import os
+import os.path
+import shutil
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
