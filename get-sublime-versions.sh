58ff7eb4b57c5e359b707393b3a5f98693614be3
Jim Lawton <jim.lawton@gmail.com>
2015-04-21 16:55:34 +0100

    Simple bash script to fetch all ST versions since 3065 (an arbitrary starting point).

---
+++ get-sublime-versions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+STARTVER=3065
+ENDVER=3084
+URLBASE="http://c758482.r82.cf2.rackcdn.com/"
+TARBASE="sublime_text_3_build_"
+DEBBASE="sublime-text_build-"
+TARSUFFIX="_x64.tar.bz2"
+DEBSUFFIX="_amd64.deb"
+ver=$STARTVER
+while [ $ver -le $ENDVER ]; do
+    url="${URLBASE}${TARBASE}${ver}${TARSUFFIX}"
+    wget $url
+    url="${URLBASE}${DEBBASE}${ver}${DEBSUFFIX}"
+    wget $url
+    ver=`expr $ver + 1`
+done
