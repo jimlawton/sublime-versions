c87a19bfc27a19cff84d83180c1ddb82b872a5e3
Jim Lawton <jim.lawton@gmail.com>
2015-07-03 13:33:02 +0100

    Take (optional) arguments for start/end versions.

--- get-sublime-versions.sh
+++ get-sublime-versions.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-STARTVER=3065
-ENDVER=3092
+STARTVER=0
+ENDVER=9999
+
+if [ "$#" -gt 0 ]; then
+    STARTVER=$1
+    if [ "$#" -gt 1 ]; then
+        ENDVER=$2
+    fi
+fi
 URLBASE="http://c758482.r82.cf2.rackcdn.com/"
 TARBASE="sublime_text_3_build_"
 DEBBASE="sublime-text_build-"
@@ -8,8 +15,12 @@ TARSUFFIX="_x64.tar.bz2"
 DEBSUFFIX="_amd64.deb"
 ver=$STARTVER
 while [ $ver -le $ENDVER ]; do
+    echo "Getting version ${ver}..."
     url="${URLBASE}${TARBASE}${ver}${TARSUFFIX}"
     wget $url
+    if [ $? != 0 ]; then
+        break
+    fi
     url="${URLBASE}${DEBBASE}${ver}${DEBSUFFIX}"
     wget $url
     ver=`expr $ver + 1`
